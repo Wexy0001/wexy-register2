59abd0b0897c943a3444c6f9f5283ee6769205e3
wexyy <106979667+Wexy0001@users.noreply.github.com>
2022-07-16 01:39:44 +0300

    wexy-register2

---
+++ wexy.js
@@ -0,0 +1,104 @@
+const { Client, Collection, Intents ,Discord ,MessageEmbed ,Permissions} = require("discord.js");
+const { interaction, MessageActionRow, MessageSelectMenu ,Message} = require('discord.js');
+const { MessageButton } = require("discord.js")
+const config = require("./config.json");
+const db = require("quick.db");
+const ms = require("pretty-ms")
+const fs = require("fs");
+const client = global.client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_PRESENCES,
+  ]
+});
+const dotenv = require("dotenv");
+dotenv.config();
+const { readdir } = require("fs");
+require("moment-duration-format");
+const commands = client.commands = new Collection();
+const aliases = client.aliases = new Collection();
+client.cooldown = new Map();
+client.commandblocked = [];
+
+require("./src/helpers/function")(client);
+
+readdir("./src/commands/", (err, files) => {
+  if (err) console.error(err)
+  files.forEach(f => {
+    readdir("./src/commands/" + f, (err2, files2) => {
+      if (err2) console.log(err2)
+      files2.forEach(file => {
+        let prop = require(`./src/commands/${f}/` + file);
+        console.log(`[Yüklenen-Komut] ${prop.name} Başarıyla yüklendi!`);
+        commands.set(prop.name, prop);
+        prop.aliases.forEach(alias => {
+          aliases.set(alias, prop.name);
+        });
+      });
+    });
+  });
+});
+
+readdir("./src/events", (err, files) => {
+  if (err) return console.error(err);
+  files.filter((file) => file.endsWith(".js")).forEach((file) => {
+    let prop = require(`./src/events/${file}`);
+    if (!prop.conf) return;
+    client.on(prop.conf.name, prop);
+    console.log(`[Yüklenen-Event] ${prop.conf.name} Başarıyla yüklendi!`);
+  });
+});
+
+client.login(config.token)
+  .then(() => console.log(`Bot ${client.user.username} olarak giriş yaptı!`))
+  .catch((err) => console.log(`Bot Giriş yapamadı sebep: ${err}`));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+client.on('message', async message => {
+if (message.content === 'wexyfakekatıl') { 
+  client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+    }
+});
+
+
+//////////////////////////////////////////////////////////////////////////////   Snipe  ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+client.on("messageDelete", async (message) => {
+    if (message.channel.type === "dm" || !message.guild || message.author.bot) return;
+    let snipe = {
+        mesaj: message.content,
+        mesajyazan: message.author.id,
+        ytarihi: message.createdTimestamp,
+        starihi: Date.now(),
+        kanal: message.channel.id
+    }
+    await db.set(`snipe.${message.guild.id}`, snipe)
+});
+
+//////////////////////////////////////////////////////////////////////////////   Komut Log  //////////////////////////////////////////////////////////////////////////////////////
+
+client.on("messageCreate", async (message) => {
+    if (message.channel.type === "dm" || !message.guild || message.author.bot) return;
+    let snipe = {
+        mesaj: message.content,
+        mesajyazan: message.author.id,
+        ytarihi: message.createdTimestamp,
+        starihi: Date.now(),
+        kanal: message.channel.id
+    }
+    await db.set(`kmtlog.${message.guild.id}`, snipe)
+});
+
+
+
+
+
+
+
+
+    
